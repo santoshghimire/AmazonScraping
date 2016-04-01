abc62e443f30d2ae1eba3129296036c4393dcb0a
Santosh Ghimire <santosh.ghimire33@gmail.com>
2016-04-01 20:25:57 +0545

    save data to mysql

--- amazonscraping/pipelines.py
+++ amazonscraping/pipelines.py
@@ -4,8 +4,65 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import MySQLdb
+from scrapy import signals
 
 
 class AmazonscrapingPipeline(object):
+
+    cnx = {
+        'host': "localhost",
+        'username': "root",
+        'password': "root",
+        'db': "amazonscraping"
+    }
+    db = None
+    cursor = None
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        pipeline = cls()
+        crawler.signals.connect(pipeline.spider_opened, signals.spider_opened)
+        crawler.signals.connect(pipeline.spider_closed, signals.spider_closed)
+        return pipeline
+
+    def spider_opened(self, spider):
+        """ Connect to RDS MySQL database when spider is opened """
+        print('** Spider opened, connecting to MySQLdb **')
+        self.db = MySQLdb.connect(
+            self.cnx['host'], self.cnx['username'],
+            self.cnx['password'], self.cnx['db']
+        )
+
+        # prepare a cursor object using cursor() method
+        self.cursor = self.db.cursor()
+
+        self.cursor.execute("SET sql_notes = 0; ")
+        self.cursor.execute(
+            """
+            create table IF NOT EXISTS amzn_products (name varchar(1000),
+            price float, rank integer, category varchar(500));
+            """
+        )
+        self.cursor.execute("SET sql_notes = 1; ")
+        print('Connection successful...')
+
+    def spider_closed(self, spider):
+        # disconnect from server
+        self.db.close()
+        print('** Spider closed **')
+
     def process_item(self, item, spider):
+        # save the item to mysql
+        print('** Saving item to MySQL **')
+        self.cursor.execute(
+            """
+            insert into amzn_products (name,price, rank, category) values
+            (%s, %s, %s, %s);
+            """,
+            (item['name'], item['price'], item['rank'], item['category'])
+        )
+        # Commit your changes in the database
+        self.db.commit()
+        print('Save successful...')
         return item
--- amazonscraping/settings.py
+++ amazonscraping/settings.py
@@ -61,9 +61,9 @@ NEWSPIDER_MODULE = 'amazonscraping.spiders'
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'amazonscraping.pipelines.SomePipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'amazonscraping.pipelines.AmazonscrapingPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
