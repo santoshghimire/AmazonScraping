e71d9c287685170bf49a7ec6e3a395014bff15b0
Santosh Ghimire <santosh.ghimire33@gmail.com>
2016-04-02 22:04:07 +0545

    Exception handling placed in item pipeline

--- amazonscraping/pipelines.py
+++ amazonscraping/pipelines.py
@@ -38,53 +38,62 @@ class AmazonscrapingPipeline(object):
             self.cnx['username'] = config['mysql']['username']
             self.cnx['password'] = config['mysql']['password']
             self.cnx['db'] = config['mysql']['db']
+        try:
+            # connect mysql
+            self.db = MySQLdb.connect(
+                self.cnx['host'], self.cnx['username'],
+                self.cnx['password'], self.cnx['db']
+            )
 
-        # connect mysql
-        self.db = MySQLdb.connect(
-            self.cnx['host'], self.cnx['username'],
-            self.cnx['password'], self.cnx['db']
-        )
+            # prepare a cursor object using cursor() method
+            self.cursor = self.db.cursor()
 
-        # prepare a cursor object using cursor() method
-        self.cursor = self.db.cursor()
-
-        self.cursor.execute("SET sql_notes = 0; ")
-        self.cursor.execute(
-            """
-            create table IF NOT EXISTS amzn_products (name varchar(1000),
-            price float, rank integer, category varchar(500),
-            url varchar(1000), shipping_weight varchar(100),
-            item_model_number varchar(200), badge_count integer,
-            amazon_seller varchar(10));
-            """
-        )
-        self.cursor.execute("SET sql_notes = 1; ")
-        print('Connection successful...')
+            self.cursor.execute("SET sql_notes = 0; ")
+            self.cursor.execute(
+                """
+                create table IF NOT EXISTS amzn_products (name varchar(1000),
+                price float, rank integer, category varchar(500),
+                url varchar(1000), shipping_weight varchar(100),
+                item_model_number varchar(200), badge_count integer,
+                amazon_seller varchar(10));
+                """
+            )
+            self.cursor.execute("SET sql_notes = 1; ")
+            print('Connection successful...')
+        except:
+            print('Connection failed !!!')
 
     def spider_closed(self, spider):
         # disconnect from server
-        self.db.close()
-        print('** Spider closed **')
+        try:
+            self.db.close()
+            print('** Spider closed, MySQL disconnected. **')
+        except:
+            print('** Spider closed. **')
 
     def process_item(self, item, spider):
         """ Process the scraped item """
-        # save the item to mysql
-        print('** Saving item to MySQL **')
-        self.cursor.execute(
-            """
-            insert into amzn_products (name, price, rank, category,
-            url, shipping_weight, item_model_number, badge_count,
-            amazon_seller) values
-            (%s, %s, %s, %s, %s, %s, %s, %s, %s);
-            """,
-            (
-                item['name'], item['price'], item['rank'], item['category'],
-                item['url'], item['shipping_weight'],
-                item['item_model_number'],
-                item['badge_count'], item['amazon_seller']
+        try:
+            # save the item to mysql
+            print('** Saving item to MySQL **')
+            self.cursor.execute(
+                """
+                insert into amzn_products (name, price, rank, category,
+                url, shipping_weight, item_model_number, badge_count,
+                amazon_seller) values
+                (%s, %s, %s, %s, %s, %s, %s, %s, %s);
+                """,
+                (
+                    item['name'], item['price'], item['rank'],
+                    item['category'],
+                    item['url'], item['shipping_weight'],
+                    item['item_model_number'],
+                    item['badge_count'], item['amazon_seller']
+                )
             )
-        )
-        # Commit your changes in the database
-        self.db.commit()
-        print('Save successful...')
+            # Commit your changes in the database
+            self.db.commit()
+            print('Save successful...')
+        except:
+            print('Save failed !!!')
         return item
